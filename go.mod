3e13c4ee82d457940444da838e210dfee8aebdb2
Brian Giori <brian.giori@amplitude.com>
2022-07-18 08:06:18 -0700

    add project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module awesomeProject
+
+go 1.12
+
+require github.com/amplitude/experiment-go-server v0.4.8
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/amplitude/experiment-go-server/pkg/experiment"
+	"github.com/amplitude/experiment-go-server/pkg/experiment/local"
+	"github.com/amplitude/experiment-go-server/pkg/experiment/remote"
+)
+
+func main() {
+	fmt.Println("asdf")
+	remoteEvaluation()
+	localEvaluation()
+}
+
+func remoteEvaluation() {
+	// (1) Initialize the local evaluation client with a server deployment key.
+	client := remote.Initialize("server-VY0FufBsdITI1Gv9y7RyUopLzk9m8t0n", &remote.Config{Debug: true})
+
+	// (2) Fetch variants for a user
+	user := &experiment.User{
+		UserId:   "user@company.com",
+		DeviceId: "abcdefg",
+		UserProperties: map[string]interface{}{
+			"premium": true,
+		},
+	}
+	variants, err := client.Fetch(user)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v\n", variants)
+
+}
+
+func localEvaluation() {
+	client := local.Initialize("server-VY0FufBsdITI1Gv9y7RyUopLzk9m8t0n", &local.Config{Debug: true})
+	err := client.Start()
+	if err != nil {
+		panic(err)
+	}
+	user := &experiment.User{UserId: "user@company.com"}
+	variants, err := client.Evaluate(user, nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", variants)
+}
